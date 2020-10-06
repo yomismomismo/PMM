406110a1505c613f43ea859d7b987a6fb338df70
puesvaleok <borjahervas54@gmail.com>
2020-10-06 16:56:23 +0200

    AE1-3: Terminado el ejercicio 3

---
+++ AE1-Actividad-Repaso-JS/AE1_4.js
@@ -0,0 +1,19 @@
+/*---- Ejercicio 4 ----*/
+
+var numero1 = 5;
+var numero2 = 8;
+
+if (numero1 < numero2) {
+    console.log("Numero1 no es mayor que numero2");
+}
+if (numero2 > 0) {
+    console.log("Numero2 es positivo");
+}
+if (numero1 < 0 || numero1 != 0) {
+    console.log("Numero1 es negativo o distinto a cero");
+}
+if (numero1 + 1 < numero2) {
+    console.log("Incrementar en 1 unidad el valor numero1 no lo hace mayor o igual que numero2");
+}
+
+/*----  ----*/
