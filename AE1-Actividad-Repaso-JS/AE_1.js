4fc64c5a164f9bb6f6b1217ce3a20e0c1a705832
puesvaleok <borjahervas54@gmail.com>
2020-10-06 15:57:39 +0200

    AE1-1: Terminado el ejercicio 1

---
+++ AE1-Actividad-Repaso-JS/AE_1.js
@@ -0,0 +1,20 @@
+
+// 1.
+
+console.log("Hola Mundo!");
+
+// A.
+
+alert("Soy el primer script");
+
+// B.
+
+let alertMessage = "Soy el primer script";
+
+alert(alertMessage);
+
+// C.
+
+const ALERT_MESSAGE = "Soy el primer script";
+
+alert(ALERT_MESSAGE + " y estoy funcionando sobre " + navigator.appVersion);
